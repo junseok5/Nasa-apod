0e55b87c7637a8ce65eab5f5d1cef4deb5c29fa7
junseok5 <vkehrkrj@naver.com>
2018-09-08 09:57:37 +0900

    Project commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#Nasa-apod project
+
+###Project stack
+- react
+- axios
+- open-color
+- moment
+- create-react-app
+- CSS Module + Sass
+
+Demo: http://shaggy-weight.surge.sh/
+Ref: https://velopert.com/3503
+
+- Demo is not supported for sass module.
---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import ViewerTemplate from './components/ViewerTemplate'
+import SpaceNavigator from './components/SpaceNavigator'
+import Viewer from './components/Viewer'
+import moment from 'moment'
+import * as api from './lib/api'
+
+class App extends Component {
+  state = {
+    loading: false,
+    maxDate: null,
+    date: null,
+    url: null,
+    mediaType: null
+  }
+
+  getAPOD = async (date) => {
+    if (this.state.loading) return
+
+    // 로딩 상태 시작
+    this.setState({
+      loading: true
+    })
+
+    try {
+      const response = await api.getAPOD(date)
+      const { date: retrievedDate, url, media_type: mediaType } = response.data
+
+      if (!this.state.maxDate) {
+        this.setState({
+          maxDate: retrievedDate
+        })
+      }
+
+      // 전달받은 데이터 넣기
+      this.setState({
+        date: retrievedDate,
+        mediaType,
+        url
+      })
+    } catch (e) {
+      console.log(e)
+    }
+
+    // 로딩 상태 종료
+    this.setState({
+      loading: false
+    })
+  }
+
+  handlePrev = () => {
+    const { date } = this.state
+    const prevDate = moment(date).subtract(1, 'days').format('YYYY-MM-DD')
+    console.log(prevDate)
+    this.getAPOD(prevDate)
+  }
+
+  handleNext = () => {
+    const { date, maxDate } = this.state
+    if (date === maxDate) return
+    const nextDate = moment(date).add(1, 'days').format('YYYY-MM-DD')
+    this.getAPOD(nextDate)
+  }
+
+  componentDidMount () {
+    this.getAPOD()
+  }
+
+  render() {
+    const { url, mediaType, loading } = this.state
+    const { handlePrev, handleNext } = this
+
+    return (
+        <ViewerTemplate
+          spaceNavigator={<SpaceNavigator onPrev={handlePrev} onNext={handleNext} />}
+          viewer={(
+            <Viewer
+              url={url}
+              mediaType={mediaType}
+              loading={loading}
+            />
+          )}
+        />
+    );
+  }
+}
+
+export default App;
